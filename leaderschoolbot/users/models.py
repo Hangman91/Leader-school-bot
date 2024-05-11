a528ae83996421ed16a951df32675a1e7f568ae5
Alexander Dementev <astalavista_91@mail.ru>
2024-05-11 11:29:26 +0300

    Начал добавлять сообщения, но пришла дочь

--- leaderschoolbot/users/models.py
+++ leaderschoolbot/users/models.py
@@ -15,3 +15,23 @@ class User(models.Model):
     class Meta:
         verbose_name = 'Профиль'
         verbose_name_plural = 'Профиль'
+
+
+class Message(models.Model):
+    user = models.ForeignKey(
+        to='users.User',
+        verbose_name='Профиль',
+        on_delete=models.PROTECT,
+    )
+    text = models.TextField(
+        verbose_name='Текст',
+    )
+
+    created_at = models.DateTimeField(
+        verbose_name='Время получения',
+        auto_now_add=True,
+    )
+
+    class Meta:
+        verbose_name = 'Сообщение'
+        verbose_name_plural = 'Сообщение'
