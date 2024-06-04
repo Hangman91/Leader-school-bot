eb3d7e40202891af3eb3fa80c7ad0ae8655b9ba2
Alexander Dementev <astalavista_91@mail.ru>
2024-06-05 00:06:49 +0500

    docker again, nginx container

--- leaderschoolbot/leaderschoolbot/settings.py
+++ leaderschoolbot/leaderschoolbot/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['172.17.48.43', 'priembot.spmi.ru', '127.0.0.1']
+ALLOWED_HOSTS = ['172.17.48.43', 'priembot.spmi.ru', 'localhost', 'admin']
 
 
 # Application definition
@@ -114,7 +114,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'Europe/Moscow'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
@@ -127,7 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
